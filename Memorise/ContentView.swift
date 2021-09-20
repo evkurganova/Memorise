f73ebecee3a5180e8dd47df48d539d75df70005e
Ekaterina Systerova <evkurganova@gmail.com>
2021-09-20 22:49:19 +0200

    first commit #L1-2

---
+++ Memorise/ContentView.swift
@@ -0,0 +1,106 @@
+//
+//  ContentView.swift
+//  Memorise
+//
+//  Created by Ekaterina Systerova on 17.09.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var emojis = ["🎾", "⚽️", "🏀", "🏈", "⚾️", "🏐", "🏉", "🎱"]
+    @State var emojisCount = 4
+    
+    var body: some View {
+        VStack {
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
+                ForEach(emojis[0..<emojisCount], id: \.self) { emoji in
+                    CardView(content: emoji)
+                        .aspectRatio(2/3, contentMode: .fit)
+                }
+            }
+            .foregroundColor(.red)
+            Spacer()
+            HStack {
+                removeButton
+                Spacer()
+                addButton
+            }
+        }
+        .font(.largeTitle)
+        .padding()
+    }
+    
+    var removeButton: some View {
+        Button {
+            if emojisCount > 0 {
+                emojisCount -= 1
+            }
+        } label: {
+            Image(systemName: "minus")
+        }
+    }
+    
+    var addButton: some View {
+        Button {
+            if emojisCount < emojis.count {
+                emojisCount += 1
+            }
+        } label: {
+            Image(systemName: "plus")
+        }
+    }
+}
+
+struct CardView: View {
+    
+    var content: String
+    @State var isFaceUp = true
+    
+    var body: some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 25.0)
+            if isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.strokeBorder(lineWidth: 2.0)
+                Text(content).font(.largeTitle)
+            } else {
+                shape.fill()
+            }
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+                .preferredColorScheme(.light)
+                .previewDevice("iPhone 12 mini")
+            ContentView()
+                .preferredColorScheme(.dark)
+                .previewDevice("iPhone 12 mini")
+        }
+            
+    }
+}
---
+++ Memorise/MemoriseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoriseApp.swift
+//  Memorise
+//
+//  Created by Ekaterina Systerova on 17.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct MemoriseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
